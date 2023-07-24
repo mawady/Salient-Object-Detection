7d6b9205f5a92c0e857341d219671b888e57d9ab
Mohamed Elawady <mohamedelsayedelawady@gmail.com>
2023-07-24 01:41:28 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,6 +2,8 @@ import tensorflow.compat.v1 as tf
 import numpy as np
 import os
 from scipy import misc
+from skimage.transform import resize
+from skimage.io import imread, imsave
 import argparse
 import sys
 
@@ -11,12 +13,20 @@ output_folder = "./test_output"
 def rgba2rgb(img):
 	return img[:,:,:3]*np.expand_dims(img[:,:,3],2)
 
+# https://stackoverflow.com/questions/53235638/how-should-i-convert-a-float32-image-to-an-uint8-image
+def normalize8(I):
+  mn = I.min()
+  mx = I.max()
+  mx -= mn
+  I = ((I - mn)/mx) * 255
+  return I.astype(np.uint8)
+
 def main(args):
 	
 	if not os.path.exists(output_folder):
 		os.mkdir(output_folder)	
 	
-	# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction = args.gpu_fraction)
+	gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction = args.gpu_fraction)
 	with tf.Session() as sess:
 		saver = tf.train.import_meta_graph('./meta_graph/my-model.meta')
 		saver.restore(sess,tf.train.latest_checkpoint('./salience_model'))
@@ -26,28 +36,29 @@ def main(args):
 		if args.rgb_folder:
 			rgb_pths = os.listdir(args.rgb_folder)
 			for rgb_pth in rgb_pths:
-				rgb = misc.imread(os.path.join(args.rgb_folder,rgb_pth))
+				rgb = np.array(imread(os.path.join(args.rgb_folder,rgb_pth))).astype(np.float32)
 				if rgb.shape[2]==4:
 					rgb = rgba2rgb(rgb)
 				origin_shape = rgb.shape
-				rgb = np.expand_dims(misc.imresize(rgb.astype(np.uint8),[320,320,3],interp="nearest").astype(np.float32)-g_mean,0)
-
+				rgb = np.expand_dims(resize(rgb.astype(np.uint8),[320,320,3]).astype(np.float32)-g_mean,0)
 				feed_dict = {image_batch:rgb}
 				pred_alpha = sess.run(pred_mattes,feed_dict = feed_dict)
-				final_alpha = misc.imresize(np.squeeze(pred_alpha),origin_shape)
-				misc.imsave(os.path.join(output_folder,rgb_pth),final_alpha)
+				final_alpha = resize(np.squeeze(pred_alpha),origin_shape)
+				imsave(os.path.join(output_folder,rgb_pth),final_alpha)
 
 		else:
-			rgb = misc.imread(args.rgb)
+			rgb = np.array(imread(args.rgb)).astype(np.float32)
 			if rgb.shape[2]==4:
 				rgb = rgba2rgb(rgb)
 			origin_shape = rgb.shape[:2]
-			rgb = np.expand_dims(misc.imresize(rgb.astype(np.uint8),[320,320,3],interp="nearest").astype(np.float32)-g_mean,0)
-
+			rgb = np.expand_dims(resize(rgb,[320,320,3]).astype(np.float32)-g_mean,0)
+			print(np.shape(rgb))
 			feed_dict = {image_batch:rgb}
 			pred_alpha = sess.run(pred_mattes,feed_dict = feed_dict)
-			final_alpha = misc.imresize(np.squeeze(pred_alpha),origin_shape)
-			misc.imsave(os.path.join(output_folder,'alpha.png'),final_alpha)
+			final_alpha = resize(np.squeeze(pred_alpha),origin_shape)
+			print(np.max(final_alpha))
+			print(np.min(final_alpha))
+			imsave(os.path.join(output_folder,'alpha.png'),normalize8(final_alpha))
 
 def parse_arguments(argv):
 	parser = argparse.ArgumentParser()
